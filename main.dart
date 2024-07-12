8d25f88ae4661749d177113f155e7492b72609b4
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-12 15:40:19 -0300

    Criando o arquivo Menu e suas opções

--- main.dart
+++ main.dart
@@ -1,2 +1,15 @@
+import 'dart:io';
 void main(){
+  menu();
+}
+
+void menu(){
+  print('======Menu Opção======');
+  print('Digite [1] para somar ');
+  print('Digite [2] para subtrair');
+  print('Digite [3] para mutiplicar');
+  print('Digite [4] para dividir');
+  print('Digite [5] para sair');
+  String? entradaDados = stdin.readLineSync();
+  
 }
