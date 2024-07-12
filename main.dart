faa49ccbc18b1afb3c5e9c2a16221b02751c0c96
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-12 16:10:41 -0300

    adicionando estrutura switch() case break defualt

--- main.dart
+++ main.dart
@@ -31,5 +31,18 @@ int menu(){
 }
 
 void escolha({required int op}){
-
+    
+    switch(op){
+      case 1:
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+      case 5:
+      break;
+      default:
+    }
 }
