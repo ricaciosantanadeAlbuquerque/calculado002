ccf20fad3b20c1a70886d00cf2d1044e743a9ba9
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-12 15:48:17 -0300

    tratamento de valores

--- main.dart
+++ main.dart
@@ -11,5 +11,20 @@ void menu(){
   print('Digite [4] para dividir');
   print('Digite [5] para sair');
   String? entradaDados = stdin.readLineSync();
-  
+
+  if(entradaDados != null){
+    if(entradaDados.isNotEmpty){
+      try{
+       int op = int.parse(entradaDados);
+      }catch(e){
+        throw Exception('ERRO!!! Não foi possível converte $entradaDados para inteiro.');
+      }
+    }else{
+      print('Digite um valor válido diferente de vazio !!');
+      exit(0);
+    }
+  }else{
+    print('Digite um valor válido !!');
+    exit(0);
+  }
 }
