25a4242e7ac1c3f0af60beb707f90c4cb035b6bf
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-12 23:19:06 -0300

    finalizando operação soma

--- main.dart
+++ main.dart
@@ -43,6 +43,7 @@ void escolha({required int op}){
            try{
              num num1 = num.parse(entrada1);
              num num2 = num.parse(entrada2);
+             print(soma(num1: num1, num2: num2));
            }catch(e){
             throw Exception('ERRO! Os valores $entrada1 e $entrada2 não podem ser convertidos.');
            }
@@ -130,6 +131,7 @@ void escolha({required int op}){
     }
 }
 
-void soma({required num num1,required num num2}){
+num soma({required num num1,required num num2}){
    var result = num1 + num2;
+   return result;
 }
