d38ac5e3352c3797038371cf6430605801f8520e
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-13 00:00:24 -0300

    função multiplicação

--- main.dart
+++ main.dart
@@ -91,6 +91,7 @@ void escolha({required int op}){
           try {
             num num1 = num.parse(entrada1);
             num num2 = num.parse(entrada2);
+            print(multiplicacao(num1: num1, num2: num2));
           } catch (e) {
             throw Exception('ERRO! Os valores $entrada1 e $entrada2 não podem ser convertidos.');
           }
@@ -142,6 +143,7 @@ num subtracao({required num num1, required num num2}){
   return result;
 }
 
-void multiplicacao({required num num1,required num num2}){
+ num multiplicacao({required num num1,required num num2}){
  var result = num1 * num2 ;
+ return result;
 }
